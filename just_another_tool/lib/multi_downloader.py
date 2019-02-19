2cea9c2b73e1e3082265edf3470cf2a4ae76a03b
NerdCoder <shajol247@gmail.com>
2019-02-19 15:16:09 +0600

    init

--- just_another_tool/lib/multi_downloader.py
+++ just_another_tool/lib/multi_downloader.py
@@ -10,13 +10,19 @@ def download():
     data, _, _ = terminal.execute(crtl_v)
     soup = bs(data, "lxml")
 
-    for link in tqdm(soup.findAll('a'), unit='files'):
+    for link in tqdm(soup.findAll('a', href=True), unit='files'):
         file_link = link.get('href')
+        print(file_link)
         file_path = urlparse(file_link).path.split('/')
         file_name = unquote(file_path[-1])
+        print(file_name)
+        
 
         cmd = "curl -o \""+ file_name + "\" " + link.get('href')
         terminal.execute(cmd)
 
 if __name__ == "__main__":
-        download()
+    download()
+
+#TODO
+"""arg to down specific files"""
